e12fcb0e3f7059ae9e32c2eb0898b2c8aeb31fe2
Luca Del Signore <lucavdelsignore@gmail.com>
2019-12-03 18:21:38 -0500

    Refactor Download and Upload modules to use OOP

--- DownloadPhotos.py
+++ DownloadPhotos.py
@@ -16,197 +16,188 @@ SCOPES = ["https://www.googleapis.com/auth/photoslibrary.readonly"]
 homeDir = expanduser("~")
 topLevelDir = homeDir + "\\Desktop\\UpfPhotoAlbums"
 
-def getPhotosService():
-	creds = None
-	# The file token.pickle stores the user's access and refresh tokens, and is
-	# created automatically when the authorization flow completes for the first
-	# time.
-	if os.path.exists('token.pickle'):
-		with open('token.pickle', 'rb') as token:
-			creds = pickle.load(token)
-	# If there are no (valid) credentials available, let the user log in.
-	if not creds or not creds.valid:
-		if creds and creds.expired and creds.refresh_token:
-			creds.refresh(Request())
+
+class Downloader(object):
+
+	def __init__(self):
+		self.googlePhotos = self.getPhotosService()
+		self.logger = MessageLogger(logName="download.log")
+
+	def getPhotosService(self):
+		creds = None
+		# The file token.pickle stores the user's access and refresh tokens, and is
+		# created automatically when the authorization flow completes for the first
+		# time.
+		if os.path.exists('token.pickle'):
+			with open('token.pickle', 'rb') as token:
+				creds = pickle.load(token)
+		# If there are no (valid) credentials available, let the user log in.
+		if not creds or not creds.valid:
+			if creds and creds.expired and creds.refresh_token:
+				creds.refresh(Request())
+			else:
+				flow = InstalledAppFlow.from_client_secrets_file(
+					'credentials.json', SCOPES)
+				creds = flow.run_local_server(port=0)
+			# Save the credentials for the next run
+			with open('token.pickle', 'wb') as token:
+				pickle.dump(creds, token)
+
+		service = build('photoslibrary', 'v1', credentials=creds)
+		return service
+
+
+	def listAllAlbums(self, pageSize=50):
+		albumsList = []
+		nextPageToken = 'Dummy'
+		while nextPageToken != '':
+			nextPageToken = '' if nextPageToken == 'Dummy' else nextPageToken
+			results = self.googlePhotos.albums().list(pageSize=pageSize, pageToken=nextPageToken).execute()
+			albums = results.get('albums', [])
+			nextPageToken = results.get('nextPageToken', '')
+			for album in albums:
+				albumsList.append(album)
+		return albumsList		
+
+	def getAlbum(self, albumId):
+		return self.googlePhotos.albums().get(albumId=albumId).execute()
+		
+	def listAlbumPhotos(self, albumId, pageSize=50):
+		photoList = []
+		nextPageToken = 'Dummy'
+		while nextPageToken != '':
+			nextPageToken = '' if nextPageToken == 'Dummy' else nextPageToken
+			results = self.googlePhotos.mediaItems().search(
+				body={"albumId": albumId, "pageSize": pageSize, "pageToken": nextPageToken}).execute()
+			photos = results.get('mediaItems', [])
+			nextPageToken = results.get('nextPageToken', '')
+			for photo in photos:
+				photoList.append(photo)
+			return photoList
+
+	def getPhoto(self, photoId):
+		return self.googlePhotos.mediaItems().get(mediaItemId=photoId).execute()
+
+	def getPhotoBinary(self, photo):
+		return self.getPhotoBinaryFromBaseUrl(photo['baseUrl'])
+
+	def getPhotoBinaryFromBaseUrl(self, baseUrl):
+		baseUrl = baseUrl + "=d"
+		res = request.urlopen(baseUrl)
+		return res.read()
+
+	def saveToFile(self, data, albumName, fileName, coverPhoto = False):
+		if coverPhoto:
+			filePath = topLevelDir + "\\" + albumName + "\\" + "Cover Photo\\" + fileName
 		else:
-			flow = InstalledAppFlow.from_client_secrets_file(
-			    'credentials.json', SCOPES)
-			creds = flow.run_local_server(port=0)
-		# Save the credentials for the next run
-		with open('token.pickle', 'wb') as token:
-			pickle.dump(creds, token)
+			filePath = topLevelDir + "\\" + albumName + "\\" + fileName
+		with open(filePath, "wb") as _file:
+			_file.write(data)
 
-	service = build('photoslibrary', 'v1', credentials=creds)
-	return service
+	def makeAlbumDir(self, albumName):
+		newDirName = topLevelDir + '\\' + albumName
+		os.mkdir(newDirName)
+
+	def makeCoverPhotoDir(self, albumName):
+		newDirName = topLevelDir + '\\' + albumName + '\\' + "Cover Photo"
+		os.mkdir(newDirName)
+
+	def makeTagsDir(self, albumName):
+		newDirName = topLevelDir + '\\' + albumName + '\\' + "Tags"
+		os.mkdir(newDirName)
+		
+	def downloadAlbum(self, albumId):
+		album = self.getAlbum(albumId)
+		albumName = album['title']
+		msg = "\tDownloading " + albumName + "..."
+		self.logger.log(msg)
+
+		coverPhotoBaseUrl = album['coverPhotoBaseUrl']
+		coverPhotoMediaItemId = album['coverPhotoMediaItemId']
+
+		self.makeAlbumDir(albumName)
+		self.makeCoverPhotoDir(albumName)
+		self.makeTagsDir(albumName)
+
+		tags = self.getAlbumTags(albumName)
+		self.writeTagsToFile(albumName, tags)
 
 
-def listAllAlbums(googlePhotos, pageSize=50):
-	albumsList = []
-	nextPageToken = 'Dummy'
-	while nextPageToken != '':
-		nextPageToken = '' if nextPageToken == 'Dummy' else nextPageToken
-		results = googlePhotos.albums().list(pageSize=pageSize, pageToken=nextPageToken).execute()
-		albums = results.get('albums', [])
-		nextPageToken = results.get('nextPageToken', '')
-		for album in albums:
-			albumsList.append(album)
-	return albumsList		
+		coverPhoto = self.getPhoto(coverPhotoMediaItemId)
+		data = self.getPhotoBinary(coverPhoto)
+		self.saveToFile(data, albumName, coverPhoto['filename'], coverPhoto=True)
 
-def getAlbum(googlePhotos, albumId):
-	return googlePhotos.albums().get(albumId=albumId).execute()
-	
-def listAlbumPhotos(googlePhotos, albumId, pageSize=50):
-	photoList = []
-	nextPageToken = 'Dummy'
-	while nextPageToken != '':
-		nextPageToken = '' if nextPageToken == 'Dummy' else nextPageToken
-		results = googlePhotos.mediaItems().search(
-			body={"albumId": albumId, "pageSize": pageSize, "pageToken": nextPageToken}).execute()
-		photos = results.get('mediaItems', [])
-		nextPageToken = results.get('nextPageToken', '')
+		photos = self.listAlbumPhotos(albumId)
+
 		for photo in photos:
-			photoList.append(photo)
-		return photoList
+			if photo['id'] != coverPhoto['id']:
+				fileName = photo['filename']
+				data = self.getPhotoBinary(photo)
+				self.saveToFile(data, albumName, fileName)
 
-def getPhoto(googlePhotos, photoId):
-	return googlePhotos.mediaItems().get(mediaItemId=photoId).execute()
+		return albumName
 
-def getPhotoBinary(photo):
-	return getPhotoBinaryFromBaseUrl(photo['baseUrl'])
+	def getAlbumTags(self, albumName):
+		tags = []
+		with open("tags.pickle", "rb") as _file:
+			tagCandidates = pickle.load(_file)
+			for key in tagCandidates:
+				if tagCandidates[key].lower() in albumName.lower():
+					if tagCandidates[key] not in tags:
+						tags.append(tagCandidates[key])
+		return tags
 
-def getPhotoBinaryFromBaseUrl(baseUrl):
-	baseUrl = baseUrl + "=d"
-	res = request.urlopen(baseUrl)
-	return res.read()
+	def writeTagsToFile(self, albumName, tags):
+		tagsDir = topLevelDir + '\\' + albumName + '\\' + "Tags\\"
+		tagsFile = tagsDir + "tags.txt"
+		with open(tagsFile, "w") as _file:
+			for tag in tags:
+				_file.write(tag + "\n")
 
-def saveToFile(data, albumName, fileName, coverPhoto = False):
-	if coverPhoto:
-		filePath = topLevelDir + "\\" + albumName + "\\" + "Cover Photo\\" + fileName
-	else:
-		filePath = topLevelDir + "\\" + albumName + "\\" + fileName
-	with open(filePath, "wb") as _file:
-		_file.write(data)
+	def getAlbums(self):
+		with open("albumList.txt") as _file:
+			albums = _file.readlines()
+			albums = [album.rstrip('\n') for album in albums]
+			albums = [album.split(' :: ') for album in albums]
+			return albums
 
-def makeAlbumDir(albumName):
-	newDirName = topLevelDir + '\\' + albumName
-	os.mkdir(newDirName)
 
-def makeCoverPhotoDir(albumName):
-	newDirName = topLevelDir + '\\' + albumName + '\\' + "Cover Photo"
-	os.mkdir(newDirName)
+	def downloadAlbums(self):
 
-def makeTagsDir(albumName):
-	newDirName = topLevelDir + '\\' + albumName + '\\' + "Tags"
-	os.mkdir(newDirName)
+
+		numAlbums = len(self.getAlbums())
+		failCount = 0
+		failList = []
+		
+		msg = "Downloading " + str(numAlbums) + " albums..."
+		self.logger.log(msg)
+
+		for album in self.getAlbums():
+			albumName = album[0]
+			albumId = album[1]
+			try:
+				self.downloadAlbum(albumId)
+				msg = "\tSuccess downloading " + albumName + "\n"
+				self.logger.log(msg)
+			except Exception as e:
+				msg = "\t" + str(e) + "\n"
+				self.logger.log(msg)
+				failCount = failCount + 1
+				failList.append(albumName)
+		
+		if failCount > 0:
+			msg = "Failed to download " + str(failCount) + " albums"
+			self.logger.log(msg)
+			for name in failList:
+				self.logger.log(name)
+		else:
+			msg = "All albums downloaded successfully"
+			self.logger.log(msg)
+
+		self.logger.endLog()
+
+
+downloader = Downloader()
+
+downloader.downloadAlbums()
 	
-def downloadAlbum(googlePhotos, albumId, logger):
-	album = getAlbum(googlePhotos, albumId)
-	albumName = album['title']
-	msg = "\tDownloading " + albumName + "..."
-	logger.log(msg)
-
-	coverPhotoBaseUrl = album['coverPhotoBaseUrl']
-	coverPhotoMediaItemId = album['coverPhotoMediaItemId']
-
-	makeAlbumDir(albumName)
-	makeCoverPhotoDir(albumName)
-	makeTagsDir(albumName)
-
-	tags = getAlbumTags(albumName)
-	writeTagsToFile(albumName, tags)
-
-
-	coverPhoto = getPhoto(googlePhotos, coverPhotoMediaItemId)
-	data = getPhotoBinary(coverPhoto)
-	saveToFile(data, albumName, coverPhoto['filename'], coverPhoto=True)
-
-	photos = listAlbumPhotos(googlePhotos, albumId)
-
-	for photo in photos:
-		if photo['id'] != coverPhoto['id']:
-			fileName = photo['filename']
-			data = getPhotoBinary(photo)
-			saveToFile(data, albumName, fileName)
-
-	return albumName
-
-def getAlbumTags(albumName):
-	tags = []
-	with open("tags.pickle", "rb") as _file:
-		tagCandidates = pickle.load(_file)
-		for key in tagCandidates:
-			if tagCandidates[key].lower() in albumName.lower():
-				if tagCandidates[key] not in tags:
-					tags.append(tagCandidates[key])
-	return tags
-
-def writeTagsToFile(albumName, tags):
-	tagsDir = topLevelDir + '\\' + albumName + '\\' + "Tags\\"
-	tagsFile = tagsDir + "tags.txt"
-	with open(tagsFile, "w") as _file:
-		for tag in tags:
-			_file.write(tag + "\n")
-
-def getAlbums(googlePhotos):
-	with open("albumList.txt") as _file:
-		albums = _file.readlines()
-		albums = [album.rstrip('\n') for album in albums]
-		albums = [album.split(' :: ') for album in albums]
-		return albums
-
-
-def downloadAlbums(googlePhotos):
-
-	logger = MessageLogger(logName="download.log")
-
-	numAlbums = len(getAlbums(googlePhotos))
-	failCount = 0
-	failList = []
-	
-	msg = "Downloading " + str(numAlbums) + " albums..."
-	logger.log(msg)
-
-	for album in getAlbums(googlePhotos):
-		albumName = album[0]
-		albumId = album[1]
-		try:
-			downloadAlbum(googlePhotos, albumId, logger)
-			msg = "\tSuccess downloading " + albumName + "\n"
-			logger.log(msg)
-		except Exception as e:
-			msg = "\t" + str(e) + "\n"
-			logger.log(msg)
-			failCount = failCount + 1
-			failList.append(albumName)
-	
-	if failCount > 0:
-		msg = "Failed to download " + str(failCount) + " albums"
-		logger.log(msg)
-		for name in failList:
-			logger.log(name)
-	else:
-		msg = "All albums downloaded successfully"
-		logger.log(msg)
-
-	logger.endLog()
-
-
-albumId = "AM4Ir-I7FCAKh6JMzG6DJ6lfDGQMDXFvqz9LS-lzey85T1KiUnOp-oWZIr__TOPTOXmu6mKmGbbl"
-#photoId = "AM4Ir-KSR_sciwARpwUtwsgmUOQXrFIX_4BP1fc19iv6If2Hx4CgzFztLBPEhZ45cr2L76qnKdoFLzIdRRMXSoP0mw9PScZx9A"
-
-#albumId = "AM4Ir-LgYgoS-JXdwMRp3s5ICyDbbehLApAPJWldevR2UWFpcoKRyyzwAL80Dnu2LoQPCAnlaKgh"
-googlePhotos = getPhotosService()
-
-
-downloadAlbums(googlePhotos)
-	
-
-'''
-photo = getPhoto(googlePhotos, photoId)
-data = getPhotoBinary(photo)
-saveToFile(data, "test.png")
-'''
-
-
-#downloadAlbum(googlePhotos, albumId)
-
--- UploadPhotos.py
+++ UploadPhotos.py
@@ -11,41 +11,16 @@ from MessageLogger import MessageLogger
 api_key = u'958d89b45e9a9a8147eacaa58fd7fba8'
 api_secret = u'f6fb0a89f6c79b28'
 
-flickr = flickrapi.FlickrAPI(api_key, api_secret)
-#flickr.authenticate_via_browser(perms='write')
-
-if not flickr.token_valid(perms='write'):
-
-	# Get a request token
-	flickr.get_request_token(oauth_callback='oob')
-
-	# Open a browser at the authentication URL. Do this however
-	# you want, as long as the user visits that URL.
-	authorize_url = flickr.auth_url(perms='write')
-	webbrowser.open_new_tab(authorize_url)
-
-	# Get the verifier code from the user. Do this however you
-	# want, as long as the user gives the application the code.
-	verifier = str(input('Verifier code: '))
-
-	# Trade the request token for an access token
-	flickr.get_access_token(verifier)
-
 homeDir = expanduser("~")
 topLevelDir = homeDir + "\\Desktop\\UpfPhotoAlbums"
 
-fileName = "5DM48204.jpg"
-photoName = fileName
-#albumName = "WS 2017 Staff-Group Posed Photos"
-albumName = "Argentina-2015-10-10-UPF Principles of Peace Presented in Buenos Aires Seminar"
-
-filePath = "C:\\Users\\lucav\\Desktop\\UpfPhotoAlbums\\WS 2017 Staff-Group Posed Photos\\5DM48204.jpg"
+def callback(progress):
+	pass
 
 class FileWithCallback(object):
 	def __init__(self, filename, callback):
 		self.file = open(filename, 'rb')
 		self.callback = callback
-		# the following attributes and methods are required
 		self.len = os.path.getsize(filename)
 		self.fileno = self.file.fileno
 		self.tell = self.file.tell
@@ -56,130 +31,152 @@ class FileWithCallback(object):
 			self.callback(self.tell() * 100 // self.len)
 		return self.file.read(size)
 
-def callback(progress):
-	pass
+class Uploader(object):
 
-def uploadPhoto(albumName, photoName, coverPhoto=False):
-	if coverPhoto:
-		filePath = topLevelDir + "\\" + albumName + "\\Cover Photo\\" + photoName
-	else:
-		filePath = topLevelDir + "\\" + albumName + "\\" + photoName
-	
-	return flickr.upload(filePath, FileWithCallback(filePath, callback))	
+	def __init__(self, api_key, api_secret):
+		self.flickr = flickrapi.FlickrAPI(api_key, api_secret)
+		self.authenticateFlickr()
+		self.api_key = api_key
+		self.logger = MessageLogger(logName="upload.log")
 
-# Add a try block in this function to handle the exception
-# of trying to use an invalid filename
-def uploadAlbum(albumName, api_key, logger):
-	
-	msg = "\tUploading " + albumName + "..."
-	logger.log(msg)
+	def authenticateFlickr(self):
+		if not self.flickr.token_valid(perms='write'):
 
-	albumPath = topLevelDir + "\\" + albumName
-	coverPath = albumPath + "\\Cover Photo"
-	tagsPath = albumPath + "\\Tags\\tags.txt"
+			# Get a request token
+			self.flickr.get_request_token(oauth_callback='oob')
 
-	with open(tagsPath, "r") as _file:
-		tags = _file.readlines()
-		tags = [tag.strip() for tag in tags]
-		tags = " ".join(tags)
+			# Open a browser at the authentication URL. Do this however
+			# you want, as long as the user visits that URL.
+			authorize_url = self.flickr.auth_url(perms='write')
+			webbrowser.open_new_tab(authorize_url)
 
-	_dir = os.listdir(coverPath)
+			# Get the verifier code from the user. Do this however you
+			# want, as long as the user gives the application the code.
+			verifier = str(input('Verifier code: '))
 
-	if len(_dir) == 0:
-		raise Exception("The \"Cover Photo\" directory is empty")
+			# Trade the request token for an access token
+			self.flickr.get_access_token(verifier)
 
-	if len(_dir) > 1:
-		raise Exception("There can only be one photo in the \"Cover Photo\" directory")
+	def uploadPhoto(self, albumName, photoName, coverPhoto=False):
+		if coverPhoto:
+			filePath = topLevelDir + "\\" + albumName + "\\Cover Photo\\" + photoName
+		else:
+			filePath = topLevelDir + "\\" + albumName + "\\" + photoName
+		
+		return self.flickr.upload(filePath, FileWithCallback(filePath, callback))	
 
-	fileName = _dir[0]
-	res = uploadPhoto(albumName, fileName, coverPhoto=True)
-	if res is None:
-		raise Exception("Failed to upload cover photo")
+	# Add a try block in this function to handle the exception
+	# of trying to use an invalid filename
+	def uploadAlbum(self, albumName):
+		
+		msg = "\tUploading " + albumName + "..."
+		self.logger.log(msg)
 
-	coverPhotoId = getPhotoId(res)
-	setPhotoTags(coverPhotoId, tags, api_key)
-	res = createAlbum(albumName, api_key, coverPhotoId)	
-	if res is None:
-		raise Exception("Failed to create album on Flickr")
+		albumPath = topLevelDir + "\\" + albumName
+		coverPath = albumPath + "\\Cover Photo"
+		tagsPath = albumPath + "\\Tags\\tags.txt"
 
-	albumId = getPhotosetId(res)
+		with open(tagsPath, "r") as _file:
+			tags = _file.readlines()
+			tags = [tag.strip() for tag in tags]
+			tags = " ".join(tags)
 
-	_dir = os.listdir(albumPath)
-	for _file in _dir:
-		if _file != "Cover Photo" and _file != "Tags":
-			res = uploadPhoto(albumName, _file)
-			photoId = getPhotoId(res)
-			setPhotoTags(photoId, tags, api_key)
-			addToAlbum(albumId, photoId, api_key)
+		_dir = os.listdir(coverPath)
 
+		if len(_dir) == 0:
+			raise Exception("The \"Cover Photo\" directory is empty")
 
-def createAlbum(albumName, api_key, primary_photo_id):
-	return flickr._flickr_call(method='flickr.photosets.create',
-							   api_key=api_key,
-							   title=albumName,
-							   primary_photo_id=primary_photo_id)
+		if len(_dir) > 1:
+			raise Exception("There can only be one photo in the \"Cover Photo\" directory")
 
-def addToAlbum(albumId, photoId, api_key):	
-	return flickr._flickr_call(method='flickr.photosets.addPhoto',
-							   api_key=api_key,
-							   photoset_id=albumId,
-							   photo_id=photoId)
+		fileName = _dir[0]
+		res = self.uploadPhoto(albumName, fileName, coverPhoto=True)
+		if res is None:
+			raise Exception("Failed to upload cover photo")
 
-def setPhotoTags(photoId, tags, api_key):
-	return flickr._flickr_call(method='flickr.photos.setTags',
-							   api_key=api_key,
-							   photo_id=photoId,
-							   tags=tags)
+		coverPhotoId = self.getPhotoId(res)
+		self.setPhotoTags(coverPhotoId, tags)
+		res = self.createAlbum(albumName, coverPhotoId)	
+		if res is None:
+			raise Exception("Failed to create album on Flickr")
 
-def getPhotoId(res):
-	return res[0].text
+		albumId = self.getPhotosetId(res)
 
-def getPhotosetId(res):
-	et = ET.fromstring(res.decode("utf-8"))
-	return et[0].attrib['id']
+		_dir = os.listdir(albumPath)
+		for _file in _dir:
+			if _file != "Cover Photo" and _file != "Tags":
+				res = self.uploadPhoto(albumName, _file)
+				photoId = self.getPhotoId(res)
+				self.setPhotoTags(photoId, tags)
+				self.addToAlbum(albumId, photoId)
 
 
-def getAlbums():
-	with open("albumList.txt") as _file:
-		albums = _file.readlines()
-		albums = [album.rstrip('\n') for album in albums]
-		albums = [album.split(' :: ')[0] for album in albums]
-		return albums
+	def createAlbum(self, albumName, primary_photo_id):
+		return self.flickr._flickr_call(method='flickr.photosets.create',
+								   api_key=self.api_key,
+								   title=albumName,
+								   primary_photo_id=primary_photo_id)
 
-def uploadAlbums(api_key):
-	
-	logger = MessageLogger(logName="upload.log")
+	def addToAlbum(self, albumId, photoId):	
+		return self.flickr._flickr_call(method='flickr.photosets.addPhoto',
+								   api_key=self.api_key,
+								   photoset_id=albumId,
+								   photo_id=photoId)
 
-	numAlbums = len(getAlbums())	
-	failCount = 0
-	failList = []
+	def setPhotoTags(self, photoId, tags):
+		return self.flickr._flickr_call(method='flickr.photos.setTags',
+								   api_key=self.api_key,
+								   photo_id=photoId,
+								   tags=tags)
+
+	def getPhotoId(self, res):
+		return res[0].text
+
+	def getPhotosetId(self, res):
+		et = ET.fromstring(res.decode("utf-8"))
+		return et[0].attrib['id']
 
 
-	msg = "Uploading " + str(numAlbums) + " albums..."
-	logger.log(msg)
+	def getAlbums(self):
+		with open("albumList.txt") as _file:
+			albums = _file.readlines()
+			albums = [album.rstrip('\n') for album in albums]
+			albums = [album.split(' :: ')[0] for album in albums]
+			return albums
 
-	for albumName in getAlbums():
-		try:
-			uploadAlbum(albumName, api_key, logger)
-			msg = "\tSuccess uploading " + albumName + "\n"
-			logger.log(msg)
-		except Exception as e:
-			msg = "\t" + str(e) + "\n"
-			logger.log(msg)
-			failCount = failCount + 1
-			failList.append(albumName)
-	
-	if failCount > 0:
-		msg = "Failed to upload " + str(failCount) + " albums"
-		logger.log(msg)
-		for name in failList:
-			logger.log(name)
-	else:
-		msg = "All albums uploaded successfully"
-		logger.log(msg)
+	def uploadAlbums(self):
+		
+		numAlbums = len(self.getAlbums())	
+		failCount = 0
+		failList = []
 
-	logger.endLog()
+		msg = "Uploading " + str(numAlbums) + " albums..."
+		self.logger.log(msg)
 
-uploadAlbums(api_key)
+		for albumName in self.getAlbums():
+			try:
+				self.uploadAlbum(albumName)
+				msg = "\tSuccess uploading " + albumName + "\n"
+				self.logger.log(msg)
+			except Exception as e:
+				msg = "\t" + str(e) + "\n"
+				self.logger.log(msg)
+				failCount = failCount + 1
+				failList.append(albumName)
+		
+		if failCount > 0:
+			msg = "Failed to upload " + str(failCount) + " albums"
+			self.logger.log(msg)
+			for name in failList:
+				self.logger.log(name)
+		else:
+			msg = "All albums uploaded successfully"
+			self.logger.log(msg)
 
+		self.logger.endLog()
+
+
+uploader = Uploader(api_key, api_secret)
+
+uploader.uploadAlbums()
 
